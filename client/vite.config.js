4ad4d7180ac3c56dd91dc421823fc2c1d6f604ba
Wildann77 <boyblanco77@gmail.com>
2025-12-02 09:10:24 +0700

    rebuid ui 5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from "path"
-import tailwindcss from "@vitejs/plugin-tailwindcss"
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
